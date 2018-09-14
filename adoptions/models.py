cb6666ec732acf7f24f6d5627faaaa65e0a023ea
lingxueli <ll2871@columbia.edu>
2018-09-13 22:54:54 -0600

    correction for typo

--- adoptions/models.py
+++ adoptions/models.py
@@ -7,7 +7,7 @@ class Pet(models.Model):
     species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30, blank = True)
     description = models.TextField()
-    sex = models.CharField(cmax_length=1, choices=SEX_CHOICES, blank = True)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank = True)
     submission_date = models.DataTimeField()
     age = models.IntegerField(null=True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
