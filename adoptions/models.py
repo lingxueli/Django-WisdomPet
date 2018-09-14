b9aaabd1de16b4f892646860ad48f0a8cc4f0dd5
lingxueli <ll2871@columbia.edu>
2018-09-13 22:58:08 -0600

    correction for typo

--- adoptions/models.py
+++ adoptions/models.py
@@ -8,7 +8,7 @@ class Pet(models.Model):
     breed = models.CharField(max_length=30, blank = True)
     description = models.TextField()
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank = True)
-    submission_date = models.DataTimeField()
+    submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
